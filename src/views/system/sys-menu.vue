44662a1ba485301527ff930ee85c40fc7e86adfd
sunxiaochun-code <13050571762@163.com>
2020-09-17 17:58:53 +0800

    菜单下拉加载

--- src/views/system/sys-menu.vue
+++ src/views/system/sys-menu.vue
@@ -9,6 +9,7 @@
             node-key="uuid"
             default-expand-all
             :expand-on-click-node="false"
+            :highlight-current="true"
             @node-click="handleNodeClick"
           >
             <span slot-scope="{ node, data }" class="custom-tree-node">
@@ -35,33 +36,38 @@
       </el-col>
       <el-col :span="12">
           <el-card shadow="hover">
-            <el-form ref="menuForm" :model="menuForm" label-width="80px" size="mini">
-            <el-form-item label="菜单名称">
-                <el-input v-model="menuForm.menuName"></el-input>
+            <el-form ref="menuForm" :model="menuForm" :rules="menuFormRules" label-width="80px" size="mini">
+            <el-form-item label="菜单名称" prop="menuName">
+                <el-input v-model.trim="menuForm.menuName"></el-input>
             </el-form-item>
-            <el-form-item label="菜单代码">
-                <el-input v-model="menuForm.code"></el-input>
+            <el-form-item label="菜单代码" prop="code">
+                <el-input v-model.trim="menuForm.code"></el-input>
             </el-form-item>
-            <el-form-item label="路径资源">
-                <el-input v-model="menuForm.url"></el-input>
+            <el-form-item label="路径资源" prop="url">
+                <el-input v-model.trim="menuForm.url"></el-input>
             </el-form-item>
-            <el-form-item label="资源类型">
-                <el-input v-model="menuForm.type"></el-input>
-            </el-form-item>
-            <el-form-item label="父级菜单">
-                <el-select v-model="menuForm.menuPid" placeholder="请选择活动区域">
-                <el-option label="区域一" value="shanghai"></el-option>
-                <el-option label="区域二" value="beijing"></el-option>
+            <el-form-item label="父级菜单" prop="menuPid">
+                <el-select v-model="menuForm.menuPid" filterable clearable placeholder="请选择父级菜单" style="width: 100%;">
+                  <el-option
+                    v-for="item in pidList"
+                    :key="item.uuid"
+                    :label="item.menuName"
+                    :value="item.uuid">
+                  </el-option>
                 </el-select>
             </el-form-item>
-            <el-form-item label="图标">
-                <el-input v-model="menuForm.icon"></el-input>
+            <el-form-item label="图标" prop="icon">
+                <el-input v-model.trim="menuForm.icon"></el-input>
             </el-form-item>
-            <el-form-item label="排序">
-                <el-input v-model="menuForm.sort"></el-input>
+            <el-form-item label="排序" prop="sort">
+                <el-input-number v-model="menuForm.sort" :min="1" :max="1000"></el-input-number>
             </el-form-item>
-            <el-form-item label="描述">
-                <el-input v-model="menuForm.description"></el-input>
+            <el-form-item label="描述" prop="description">
+                <el-input v-model.trim="menuForm.description"></el-input>
+            </el-form-item>
+            <el-form-item right>
+              <el-button type="primary" @click="onSubmit">提交</el-button>
+              <el-button>取消</el-button>
             </el-form-item>
             </el-form>
             </el-card>
@@ -80,7 +86,31 @@ export default {
         children: 'childMenu'
       },
       menuData: [],
-      menuForm:{}
+      menuForm:{},
+      menuFormRules:{
+        menuName: [
+            { required: true, message: '请输入菜单名称', trigger: 'blur' },
+            { min: 1, max: 50, message: '长度在 1 到 50 个字符', trigger: 'blur' }
+          ],
+          code: [
+            { required: true, message: '请输入菜单代码', trigger: 'blur' },
+            { min: 1, max: 50, message: '长度在 1 到 50 个字符', trigger: 'blur' }
+          ],
+          url: [
+            { required: true, message: '请输入资源路径', trigger: 'blur' },
+            { min: 1, max: 100, message: '长度在 1 到 100 个字符', trigger: 'blur' }
+          ],
+          menuPid: [
+            { required: true, message: '请选择父级菜单', trigger: 'blur' },
+          ],
+          icon: [
+            { min: 1, max: 50, message: '长度在 1 到 50 个字符', trigger: 'blur' }
+          ],
+          description: [
+            { min: 1, max: 200, message: '长度在 1 到 200 个字符', trigger: 'blur' }
+          ],
+      },
+      pidList:[]
     }
   },
   created() {
@@ -108,8 +138,11 @@ export default {
     },
     getMenu(){
        getMenu({}).then(response => {
-        console.log(response)
+         this.pidList = response.data.items
       }) 
+    },
+    onSubmit(){
+
     }
   }
 }
